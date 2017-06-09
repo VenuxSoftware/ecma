2abe09b3deefa7d177af00e21a69193b855ee592
Eugene Nosko <coder@venux.com>
2017-06-08 20:22:44 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,109 +1,98 @@
+'use strict'
+var fs = require('graceful-fs')
+var path = require('path')
+var chain = require('slide').chain
+var iferr = require('iferr')
+var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
+var rmStuff = require('../../unbuild.js').rmStuff
+var lifecycle = require('../../utils/lifecycle.js')
+var updatePackageJson = require('../update-package-json.js')
+var move = require('../../utils/move.js')
+
 /*
-  Status: prototype
-  Process: API generation
+  Move a module from one point in the node_modules tree to another.
+  Do not disturb either the source or target location's node_modules
+  folders.
 */
 
-var speakWorker;
-try {
-  speakWorker = new Worker('speakWorker.js');
-} catch(e) {
-  console.log('speak.js warning: no worker support');
+module.exports = function (staging, pkg, log, next) {
+  log.silly('move', pkg.fromPath, pkg.path)
+  chain([
+    [lifecycle, pkg.package, 'preuninstall', pkg.fromPath, false, true],
+    [lifecycle, pkg.package, 'uninstall', pkg.fromPath, false, true],
+    [rmStuff, pkg.package, pkg.fromPath],
+    [lifecycle, pkg.package, 'postuninstall', pkg.fromPath, false, true],
+    [moveModuleOnly, pkg.fromPath, pkg.path, log],
+    [lifecycle, pkg.package, 'preinstall', pkg.path, false, true],
+    [removeEmptyParents, path.resolve(pkg.fromPath, '..')],
+    [updatePackageJson, pkg, pkg.path]
+  ], next)
 }
 
-function speak(text, args) {
-  var PROFILE = 1;
+function removeEmptyParents (pkgdir, next) {
+  fs.rmdir(pkgdir, function (er) {
+    // FIXME: Make sure windows does what we want here
+    if (er && er.code !== 'ENOENT') return next()
+    removeEmptyParents(path.resolve(pkgdir, '..'), next)
+  })
+}
 
-  function parseWav(wav) {
-    function readInt(i, bytes) {
-      var ret = 0;
-      var shft = 0;
-      while (bytes) {
-        ret += wav[i] << shft;
-        shft += 8;
-        i++;
-        bytes--;
-      }
-      return ret;
-    }
-    if (readInt(20, 2) != 1) throw 'Invalid compression code, not PCM';
-    if (readInt(22, 2) != 1) throw 'Invalid number of channels, not 1';
-    return {
-      sampleRate: readInt(24, 4),
-      bitsPerSample: readInt(34, 2),
-      samples: wav.subarray(44)
-    };
-  }
+function moveModuleOnly (from, to, log, done) {
+  var fromModules = path.join(from, 'node_modules')
+  var tempFromModules = from + '.node_modules'
+  var toModules = path.join(to, 'node_modules')
+  var tempToModules = to + '.node_modules'
 
-  function playHTMLAudioElement(wav) {
-    function encode64(data) {
-      var BASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-      var PAD = '=';
-      var ret = '';
-      var leftchar = 0;
-      var leftbits = 0;
-      for (var i = 0; i < data.length; i++) {
-        leftchar = (leftchar << 8) | data[i];
-        leftbits += 8;
-        while (leftbits >= 6) {
-          var curr = (leftchar >> (leftbits-6)) & 0x3f;
-          leftbits -= 6;
-          ret += BASE[curr];
-        }
-      }
-      if (leftbits == 2) {
-        ret += BASE[(leftchar&3) << 4];
-        ret += PAD + PAD;
-      } else if (leftbits == 4) {
-        ret += BASE[(leftchar&0xf) << 2];
-        ret += PAD;
-      }
-      return ret;
-    }
+  log.silly('move', 'move existing destination node_modules away', toModules)
 
-    document.getElementById("audio").innerHTML=("<audio id=\"player\" src=\"data:audio/x-wav;base64,"+encode64(wav)+"\">");
-    document.getElementById("player").play();
-  }
+  move(toModules, tempToModules, removeDestination(done))
 
-  function playAudioDataAPI(data) {
-    try {
-      var output = new Audio();
-      output.mozSetup(1, data.sampleRate);
-      var num = data.samples.length;
-      var buffer = data.samples;
-      var f32Buffer = new Float32Array(num);
-      for (var i = 0; i < num; i++) {
-        var value = buffer[i<<1] + (buffer[(i<<1)+1]<<8);
-        if (value >= 0x8000) value |= ~0x7FFF;
-        f32Buffer[i] = value / 0x8000;
+  function removeDestination (next) {
+    return function (er) {
+      log.silly('move', 'remove existing destination', to)
+      if (er) {
+        rimraf(to, iferr(next, makeDestination(next)))
+      } else {
+        rimraf(to, iferr(next, makeDestination(iferr(next, moveToModulesBack(next)))))
       }
-      output.mozWriteAudio(f32Buffer);
-      return true;
-    } catch(e) {
-      return false;
     }
   }
 
-  function handleWav(wav) {
-    var startTime = Date.now();
-    var data = parseWav(wav); // validate the data and parse it
-    // TODO: try playAudioDataAPI(data), and fallback if failed
-    playHTMLAudioElement(wav);
-    if (PROFILE) console.log('speak.js: wav processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
+  function moveToModulesBack (next) {
+    return function () {
+      log.silly('move', 'move existing destination node_modules back', toModules)
+      move(tempToModules, toModules, iferr(done, next))
+    }
   }
 
-  if (args && args.noWorker) {
-    // Do everything right now. speakGenerator.js must have been loaded.
-    var startTime = Date.now();
-    var wav = generateSpeech(text, args);
-    if (PROFILE) console.log('speak.js: processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
-    handleWav(wav);
-  } else {
-    // Call the worker, which will return a wav that we then play
-    var startTime = Date.now();
-    speakWorker.onmessage = function(event) {
-      if (PROFILE) console.log('speak.js: worker processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
-      handleWav(event.data);
-    };
-    speakWorker.postMessage({ text: text, args: args });
+  function makeDestination (next) {
+    return function () {
+      log.silly('move', 'make sure destination parent exists', path.resolve(to, '..'))
+      mkdirp(path.resolve(to, '..'), iferr(done, moveNodeModules(next)))
+    }
+  }
+
+  function moveNodeModules (next) {
+    return function () {
+      log.silly('move', 'move source node_modules away', fromModules)
+      move(fromModules, tempFromModules, iferr(doMove(next), doMove(moveNodeModulesBack(next))))
+    }
+  }
+
+  function doMove (next) {
+    return function () {
+      log.silly('move', 'move module dir to final dest', from, to)
+      move(from, to, iferr(done, next))
+    }
+  }
+
+  function moveNodeModulesBack (next) {
+    return function () {
+      mkdirp(from, iferr(done, function () {
+        log.silly('move', 'put source node_modules back', fromModules)
+        move(tempFromModules, fromModules, iferr(done, next))
+      }))
+    }
   }
 }
